e70d7f193b9e1d41ba406e4c32f43ae63b16cf05
Alan Farrell <alanfarrell031083@gmail.com>
2026-02-11 11:53:17 +0000

    Update site 2026-02-11_11-53

--- build.sh
+++ build.sh
@@ -12,8 +12,6 @@ fi
 echo "Generating game pages..."
 python3 tools/generate_game_pages.py
 
-echo "Generating RSS feed..."
-python3 tools/generate_rss_feed.py
 
 echo "Generating series pages..."
 python3 tools/generate_series_pages.py
